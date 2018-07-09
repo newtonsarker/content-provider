a9a104a3a102bd2ffc1457f91656f8f2f7281af4
Newton Sarker <newtonsarker@gmail.com>
2018-07-10 02:51:57 +0600

    added logback and configured it

--- content-service/src/main/java/com/fortumo/bahrain/content/service/ContentService.java
+++ content-service/src/main/java/com/fortumo/bahrain/content/service/ContentService.java
@@ -7,10 +7,10 @@ public class ContentService {
     public static void main(String[] args) {
         DBConnection.createTables();
 
-        PaymentNotificationReaderTask notificationTask = new PaymentNotificationReaderTask(5);
+        PaymentNotificationReaderTask notificationTask = new PaymentNotificationReaderTask(10);
         notificationTask.start();
 
-        ContentDeliveryTask deliveryTask = new ContentDeliveryTask(6);
+        ContentDeliveryTask deliveryTask = new ContentDeliveryTask(15);
         deliveryTask.start();
     }
 
